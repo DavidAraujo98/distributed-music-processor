1282be652f16146fc8ae0fabbc222cef071a95d9
David Araújo <david2araujo5@gmail.com>
2023-06-12 17:17:01 +0100

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -2,39 +2,40 @@
 
 # sudo ./run_server.sh [--init]
 
-echo -e "[-] Cleaning all received and produced files"
+echo -e "\n[-] Cleaning all received and produced files"
 sudo rm -r uploads
 sudo rm -r download
 
-echo -e "[-] Cleaning rabbitmq channels..." 
-sudo rabbitmqctl purge_queue job_queue &>/dev/null
-sudo rabbitmqctl purge_queue result_queue &>/dev/null
+echo -e "\n[-] Cleaning rabbitmq channels..." 
+sudo rabbitmqctl purge_queue job_queue
+sudo rabbitmqctl purge_queue result_queue
 
-echo "[+] Creating environment..."
-python3 -m venv venv    &>/dev/null
-source venv/bin/activate    &>/dev/null
+echo -e "\n[+] Creating environment..."
+sudo apt install python3.11-venv -y
+python3 -m venv venv
+source venv/bin/activate
 
 if [[ $1 == "--init" ]];then
-    echo "[+] Installing dependencies..."
-    sudo apt-get update &>/dev/null
-    sudo apt install ffmpeg &>/dev/null
-    sudo apt-get install rabbitmq-server -y --fix-missing &>/dev/null
-    sudo systemctl start rabbitmq-server &>/dev/null
+    echo -e "\n[+] Installing dependencies..."
+    sudo apt-get update
+    sudo apt install ffmpeg -y
+    sudo apt-get install rabbitmq-server -y --fix-missing
+    sudo systemctl start rabbitmq-server
 
-    echo "[+] Installing pip dependencies"
-    pip install pip --upgrade &>/dev/null
-    pip install -r requirements_torch.txt &>/dev/null
-    pip install -r requirements_demucs.txt &>/dev/null
-    pip install -r requirements_api.txt &>/dev/null
+    echo -e "\n[+] Installing pip dependencies"
+    pip install pip --upgrade
+    pip install -r requirements_torch.txt
+    pip install -r requirements_demucs.txt
+    pip install -r requirements_api.txt
 
-    echo "[+] Starting rabbitmq server..."
+    echo -e "\n[+] Starting rabbitmq server..."
     sudo rabbitmq-server start -detached & 
     sleep 2
 fi
 
-echo "[+] Starting API at 0.0.0.0:8000..."
+echo -e "\n[+] Starting API at 0.0.0.0:8000..."
 python3 api.py
 
-echo -e "[-] Cleaning all received and produced files"
+echo -e "\n[-] Cleaning all received and produced files"
 sudo rm -r uploads
 sudo rm -r download
